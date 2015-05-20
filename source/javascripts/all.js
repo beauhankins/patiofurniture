3185f3423a6bf9c48ac2b3ff65a407c10706c314
Beau Hankins <beau.j.hankins@gmail.com>
2015-05-20 12:46:40 +1000

    initial layout and styling with placeholders

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,12 @@
-//= require_tree .
+$(function() {
+  // Masonry
+  var $grid = $('#grid');
+  $grid.imagesLoaded(function() {
+    $grid.masonry({
+      // Options
+      itemSelector : '.grid-item',
+      columnWidth : $(".item").width(),
+      gutter: 0
+    });
+  });
+});
